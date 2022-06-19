03f936093e9c93ed3b62d835d6f13e15d6e5af32
gerryflap <gerry2211@gmail.com>
2022-06-19 11:59:40 +0200

    Added UNet-like mnist model. CPU training on laptop shows potential.

--- mnist_diffusion_model.py
+++ mnist_diffusion_model.py
@@ -7,10 +7,8 @@
 
 import torch
 import torchvision.utils
-from torch.nn import Linear, Tanh
-from torch.nn.modules import Sequential
 from torch.optim.rmsprop import RMSprop
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 
@@ -18,7 +16,9 @@ import diffusion_models as dm
 
 # === Hyper parameters for this experiment ===
 # Hidden layer size
-h_size = 512
+from mnist_models import UNetMNIST
+
+h_size = 8
 # The number of samples per mode
 n_samples_per_mode = 1000
 # Epochs to train for (an epoch is 1 training run over the dataset)
@@ -32,13 +32,15 @@ T = 1000
 # Number of evaluation samples
 n_eval_samples = 64
 
-# === Define the prediction model as a simple neural network ===
-model = Sequential(
-    Linear(28*28 + 1, h_size),
-    Tanh(),
+# === Define the prediction model ===
+# model = Sequential(
+#     Linear(28*28 + 1, h_size),
+#     Tanh(),
+#
+#     Linear(h_size, 28*28),
+# )
+model = UNetMNIST(h_size)
 
-    Linear(h_size, 28*28),
-)
 
 opt = RMSprop(model.parameters(), lr)
 
@@ -58,16 +60,15 @@ dataloader = DataLoader(dataset, batch_size, shuffle=True, drop_last=True)
 try:
     for epoch in range(epochs):
         for x, _ in dataloader:
-            x = x.view(-1, 28*28)
             opt.zero_grad()
             t = torch.floor(torch.rand((batch_size, 1)) * (T - 1) + 1)
-            eps = torch.normal(0, 1, (batch_size, 28*28))
-            alpha_cumulative_t = dm.alpha_hat(t, T)
+            eps = torch.normal(0, 1, (batch_size, 1, 28, 28))
+            alpha_cumulative_t = dm.alpha_hat(t, T).view(-1, 1, 1, 1)
 
             x_after = torch.sqrt(alpha_cumulative_t) * x
             x_after += torch.sqrt(1.0 - alpha_cumulative_t) * eps
-            model_inp = torch.cat([x_after, t / T], dim=1)
-            prediction = model(model_inp)
+            # model_inp = torch.cat([x_after, t / T], dim=1)
+            prediction = model(x_after, t/T)
 
             loss = torch.mean(torch.square(eps - prediction))
 
@@ -79,19 +80,19 @@ except KeyboardInterrupt:
     print("Training interrupted, showing results")
 
 # === Evaluation ===
-x_T = torch.normal(0, 1, (n_eval_samples, 28*28))
+x_T = torch.normal(0, 1, (n_eval_samples, 1, 28, 28))
 ones = torch.ones((n_eval_samples, 1), dtype=torch.float32)
 x_t = x_T
 for t_val in range(T, 1, -1):
     t = ones * t_val
-    alpha_cumulative_t = dm.alpha_hat(t, T)
+    alpha_cumulative_t = dm.alpha_hat(t, T).view(-1, 1, 1, 1)
     alpha_cumulative_t[alpha_cumulative_t == 0.0] = 1e-6
-    beta = dm.beta(t, T)
+    beta = dm.beta(t, T).view(-1, 1, 1, 1)
 
-    model_inp = torch.cat([x_t, t / float(T)], dim=1)
-    pred = model(model_inp)
+    # model_inp = torch.cat([x_t, t / float(T)], dim=1)
+    pred = model(x_t, t / float(T))
     x_prev = (x_t - (beta / (torch.sqrt(1.0 - alpha_cumulative_t))) * pred) / (torch.sqrt(1.0 - beta))
-    x_prev += sigma(t) * torch.randn_like(x_t)
+    x_prev += sigma(t).view(-1, 1, 1, 1) * torch.randn_like(x_t)
 
     # Set x t to x t-1 (and clamp the values to known ranges so everything stays in line
     x_t = torch.clamp(x_prev, -5, 5)
